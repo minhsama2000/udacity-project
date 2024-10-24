2d680b7a338c22d2f81526d342e66d7fb7475922
minhsama <minhsama2000@gmail.com>
2024-10-24 11:33:28 +0700

    done

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a8dc1df70b17a4296bd02727e9621a02-66501870.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
